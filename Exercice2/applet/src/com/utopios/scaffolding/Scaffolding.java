949a0b214882cbf4cd7da7647f074830deb0b324
unknown <ihab@utopios.net>
2025-03-17 15:21:39 +0100

    avec code correction exercice 2

---
+++ Exercice2/applet/src/com/utopios/scaffolding/Scaffolding.java
@@ -0,0 +1,169 @@
+
+package com.utopios.scaffolding;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.Util;
+import javacard.framework.OwnerPIN;
+
+public class Scaffolding extends Applet {
+    
+    private static final byte INS_VERIFY_PIN  = (byte) 0x20;
+    private static final byte INS_CHANGE_PIN  = (byte) 0x21;
+    private static final byte INS_UNBLOCK_PIN = (byte) 0x55;
+
+    private static final byte PIN_MAX_TRIES = 3;
+    private static final byte PIN_MAX_SIZE  = 4;
+
+    private static final byte PUK_MAX_TRIES = 5;
+    private static final byte PUK_SIZE      = 8;
+    
+    private static final short SW_CARD_BLOCKED = (short) 0x6283;
+    private static final short SW_PIN_BLOCKED  = (short) 0x6983;
+
+    private OwnerPIN pin;
+
+    private byte[] pukValue;
+    private byte pukTriesRemaining;
+
+    private boolean cardBlocked;
+    
+    protected Scaffolding() {
+        pin = new OwnerPIN(PIN_MAX_TRIES, PIN_MAX_SIZE);
+        byte[] defaultPin = { '1','2','3','4' };
+        pin.update(defaultPin, (short) 0, (byte) defaultPin.length);
+
+        pukValue = new byte[PUK_SIZE];
+        byte[] defaultPuk = { '1','2','3','4','5','6','7','8' };
+        Util.arrayCopyNonAtomic(defaultPuk, (short)0, pukValue, (short)0, (short)defaultPuk.length);
+        pukTriesRemaining = PUK_MAX_TRIES;
+
+        cardBlocked = false;
+        register();
+    }
+
+    
+    public static void install(byte[] bArray, short bOffset, byte bLength) {
+        new Scaffolding();
+    }
+
+   
+   @Override 
+   public void process(APDU apdu) {
+        if (selectingApplet()) {
+            return;
+        }
+        byte[] buffer = apdu.getBuffer();
+        byte ins = buffer[ISO7816.OFFSET_INS];
+
+        if (cardBlocked) {
+            ISOException.throwIt(SW_CARD_BLOCKED);
+        }
+
+        switch(ins) {
+            case INS_VERIFY_PIN:
+                verifyPin(apdu);
+                break;
+            case INS_CHANGE_PIN:
+                changePin(apdu);
+                break;
+            case INS_UNBLOCK_PIN:
+                break;
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        }
+   }
+
+   private void verifyPin(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte lc = buffer[ISO7816.OFFSET_LC];       
+        short dataOffset = ISO7816.OFFSET_CDATA;
+
+        if (lc == 0 || lc > PIN_MAX_SIZE) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+
+        // Tenter la vérification
+        boolean pinOk = pin.check(buffer, dataOffset, lc);
+        if (!pinOk) {
+            // PIN incorrect => pin.check() décrémente déjà le compteur
+            // Si le compteur d'essais PIN atteint 0, le PIN est bloqué
+            if (pin.getTriesRemaining() == 0) {
+                // PIN bloqué => on peut renvoyer un SW explicite
+                ISOException.throwIt(SW_PIN_BLOCKED);
+            } else {
+                ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+            }
+        }
+        // Si on arrive ici, le PIN est vérifié et "authentifié"
+    }
+
+    private void changePin(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte lc = buffer[ISO7816.OFFSET_LC];
+        short dataOffset = ISO7816.OFFSET_CDATA;
+
+        // Vérifier que la session est authentifiée (PIN validé)
+        if (!pin.isValidated()) {
+            ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+        }
+
+        // Vérifier la longueur du nouveau PIN
+        if (lc == 0 || lc > PIN_MAX_SIZE) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+
+        // Mettre à jour la valeur du PIN
+        pin.update(buffer, dataOffset, lc);
+
+        
+        // nouvelle vérification la prochaine fois
+        pin.reset();
+    }
+
+    private void unblockPin(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte lc = buffer[ISO7816.OFFSET_LC];
+        short dataOffset = ISO7816.OFFSET_CDATA;
+
+        // Vérifier la longueur de PUK reçue
+        if (lc != PUK_SIZE) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+
+        // Vérifier si le compteur de tentatives PUK n'est pas déjà à 0
+        if (pukTriesRemaining == 0) {
+            // La carte est déjà bloquée à cause du PUK épuisé
+            // On met "cardBlocked" à true pour bloquer toutes les opérations ultérieures
+            cardBlocked = true;
+            ISOException.throwIt(SW_CARD_BLOCKED);
+        }
+
+        // Comparer le PUK reçu au PUK stocké
+        if (Util.arrayCompare(buffer, dataOffset, pukValue, (short)0, (short)PUK_SIZE) == 0) {
+            // PUK correct => débloquer le PIN
+            pin.resetAndUnblock();
+
+            // Réinitialiser le PIN à une valeur par défaut (ex: "1234")
+            byte[] newPin = { '1','2','3','4' };
+            pin.update(newPin, (short) 0, (byte) newPin.length);
+
+            // Restaurer le compteur de tentatives PUK
+            pukTriesRemaining = PUK_MAX_TRIES;
+
+        } else {
+            // PUK incorrect => décrémenter et vérifier si on arrive à 0
+            pukTriesRemaining--;
+            if (pukTriesRemaining == 0) {
+                // Bloquer la carte
+                cardBlocked = true;
+                ISOException.throwIt(SW_CARD_BLOCKED);
+            // } else {
+                // Sinon, on lève un SW "sécurité non satisfaite"
+                ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+            }
+        }
+    }
+}
---
+++ Exercice2/client/src/com/utopios/client/ScaffoldingClient.java
@@ -0,0 +1,264 @@
+
+package com.utopios.client;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.IntStream;
+
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import javax.smartcardio.TerminalFactory;
+
+import com.oracle.javacard.ams.AMService;
+import com.oracle.javacard.ams.AMServiceFactory;
+import com.oracle.javacard.ams.AMSession;
+import com.oracle.javacard.ams.config.AID;
+import com.oracle.javacard.ams.config.CAPFile;
+import com.oracle.javacard.ams.script.APDUScript;
+import com.oracle.javacard.ams.script.ScriptFailedException;
+import com.oracle.javacard.ams.script.Scriptable;
+import java.util.Scanner;
+
+public class ScaffoldingClient {
+
+	static final String isdAID = "aid:A000000151000000";
+	static final String sAID_CAP = "aid:a00000006203010C01";
+	static final String sAID_AppletClass = "aid:a00000006203010C0101";
+	static final String sAID_AppletInstance = "aid:a00000006203010C0101";
+    static final CommandAPDU selectApplet = new CommandAPDU(0x00, 0xA4, 0x04, 0x00, AID.from(sAID_AppletInstance).toBytes(), 256);
+
+	/**
+	 * Launch the sample
+	 *
+	 * @param args command arguments. Use {@code -cap=<capfile> -props=<propsfile>}
+	 */
+	public static void main(String[] args) {
+
+		int iResult = 0;
+
+		try {
+			CAPFile appFile = CAPFile.from(getArg(args, "cap"));
+			Properties props = new Properties();
+			props.load(new FileInputStream(getArg(args, "props")));
+
+			// Create and configure Application Management Service
+			AMService ams = AMServiceFactory.getInstance("GP2.2");
+			ams.setProperties(props);
+			for (String key : ams.getPropertiesKeys()) {
+				System.out.println(key + " = " + ams.getProperty(key));
+			}
+
+			 // Application Management session used to deploy CAPFile
+            AMSession deploy = ams.openSession(isdAID)   // select SD & open secure channel
+                    .load(sAID_CAP, appFile.getBytes())  // load an application file
+                    .install(sAID_CAP,                   // install application
+                             sAID_AppletClass, sAID_AppletInstance)
+                    .close();
+
+            // Application Management session used to undeploy CAPFile
+            AMSession undeploy = ams.openSession(isdAID) // select SD & open secure channel
+                    .uninstall(sAID_AppletInstance)      // uninstall the application
+                    .unload(sAID_CAP)                    // unload the application code
+                    .close();
+
+			// sample script: deploy an Applet, use it and undeploy it.
+			TestScript testScript = new TestScript()
+					.append( deploy )
+	                .append( selectApplet );
+			// Terminal to simulator
+			CardTerminal t = getTerminal("socket", "127.0.0.1", "9025"); // or getTerminal("pcsc");
+
+			// Wait some seconds to allow connections
+			if (t.waitForCardPresent(10000)) {
+				System.out.println("Connection to simulator established: "+ t.getName());
+				Card c = t.connect("*");
+				System.out.println(getFormattedATR(c.getATR().getBytes()));
+
+				
+
+                
+
+                System.out.println("Entering interactive mode. Type 'exit' to quit.");
+                /*CommandAPDU selectApplet1 = new CommandAPDU(0x00, 0xA4, 0x04, 0x00, AID.from(sAID_AppletInstance).toBytes());
+                ResponseAPDU response1 = c.getBasicChannel().transmit(selectApplet1);
+                printResponse(response1);*/
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    System.out.print("Enter APDU command: ");
+                    String input = scanner.nextLine();
+                    if (input.equalsIgnoreCase("exit")) {
+                        break;
+                    }
+
+                    try {
+                        CommandAPDU commandAPDU = parseCommandAPDU(input);
+                        testScript.append(commandAPDU);
+                    } catch (Exception e) {
+                        System.out.println("Invalid APDU command. Try again.");
+                    }
+                }
+                scanner.close();
+                testScript.append(undeploy);
+
+                List<ResponseAPDU> responses = testScript.run(c.getBasicChannel());
+				c.disconnect(true);
+
+				System.out.println("Responses count: " + responses.size());
+			}
+			else {
+				System.out.println("Connection to simulator failed");
+				iResult = -1;
+			}
+
+		} catch (NoSuchAlgorithmException | NoSuchProviderException | CardException | ScriptFailedException | IOException e) {
+			e.printStackTrace();
+			iResult = -1;
+		}
+		System.exit (iResult);
+	}
+
+    private static void printResponse(ResponseAPDU response) {
+        byte[] data = response.getData();
+        StringBuilder sb = new StringBuilder("Response: ");
+        for (byte b : data) {
+            sb.append(String.format("%02X ", b));
+        }
+        sb.append(String.format("SW: %04X", response.getSW()));
+        System.out.println(sb.toString());
+    }
+
+    private static CommandAPDU parseCommandAPDU(String input) {
+        String[] parts = input.trim().split(" ");
+        byte[] commandBytes = new byte[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            commandBytes[i] = (byte) Integer.parseInt(parts[i], 16);
+        }
+        return new CommandAPDU(commandBytes);
+    }
+
+
+
+	private static String getArg(String[] args, String argName) throws IllegalArgumentException {
+        String value = null;
+
+        for (String param : args) {
+            if (param.startsWith("-" + argName + "=")) {
+                value = param.substring(param.indexOf('=') + 1);
+            }
+        }
+
+        if(value == null || value.length() == 0) {
+            throw new IllegalArgumentException("Argument " + argName + " is missing");
+        }
+
+        return value;
+    }
+
+    /**
+     * Puts all ATR bytes into a single string using hexadecimal format
+     * @param ATR ATR bytes
+     * @return Formatted ATR
+     */
+    private static String getFormattedATR(byte[] ATR) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : ATR) {
+            sb.append(String.format("%02X ", b));
+        }
+        return String.format("ATR: [%s]", sb.toString().trim());
+    }
+
+	private static CardTerminal getTerminal(String... connectionParams) throws NoSuchAlgorithmException, NoSuchProviderException, CardException {
+        TerminalFactory tf;
+        String connectivityType = connectionParams[0];
+        if (connectivityType.equals("socket")) {
+            String ipaddr = connectionParams[1];
+            String port = connectionParams[2];
+            tf = TerminalFactory.getInstance("SocketCardTerminalFactoryType",
+                    List.of(new InetSocketAddress(ipaddr, Integer.parseInt(port))),
+                    "SocketCardTerminalProvider");
+        } else {
+            tf = TerminalFactory.getDefault();
+        }
+        return tf.terminals().list().get(0);
+    }
+
+	private static class TestScript extends APDUScript {
+        private List<CommandAPDU>  commands = new LinkedList<>();
+        private List<ResponseAPDU> responses = new LinkedList<>();
+        private int index = 0;
+
+        public List<ResponseAPDU> run(CardChannel channel) throws ScriptFailedException {
+            return super.run(channel, c -> lookupIndex(c), r -> !isExpected(r));
+        }
+
+        @Override
+        public TestScript append(Scriptable<CardChannel, CommandAPDU, ResponseAPDU> other) {
+            super.append(other);
+            return this;
+        }
+
+        public TestScript append(CommandAPDU apdu, ResponseAPDU expected) {
+            super.append(apdu);
+            this.commands.add(apdu);
+            this.responses.add(expected);
+            return this;
+        }
+
+        public TestScript append(CommandAPDU apdu) {
+            super.append(apdu);
+            return this;
+        }
+
+        private CommandAPDU lookupIndex(CommandAPDU apdu) {
+            print(apdu);
+            this.index = IntStream.range(0, this.commands.size())
+                    .filter(i -> apdu == this.commands.get(i))
+                    .findFirst()
+                    .orElse(-1);
+            return apdu;
+        }
+
+        private boolean isExpected(ResponseAPDU response) {
+
+            ResponseAPDU expected = (index < 0)? response : this.responses.get(index);
+            if (!response.equals(expected)) {
+                System.out.println("Received: ");
+                print(response);
+                System.out.println("Expected: ");
+                print(expected);
+                return false;
+            }
+            print(response);
+            return true;
+        }
+
+        private static void print(CommandAPDU apdu) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("%02X%02X%02X%02X %02X[", apdu.getCLA(), apdu.getINS(), apdu.getP1(), apdu.getP2(), apdu.getNc()));
+            for (byte b : apdu.getData()) {
+                sb.append(String.format("%02X", b));
+            }
+            sb.append("]");
+            System.out.format("[%1$tF %1$tT %1$tL %1$tZ] [APDU-C] %2$s %n", System.currentTimeMillis(), sb.toString());
+        }
+
+        private static void print(ResponseAPDU apdu) {
+            byte[] bytes = apdu.getData();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02X", b));
+            }
+            System.out.format("[%1$tF %1$tT %1$tL %1$tZ] [APDU-R] [%2$s] SW:%3$04X %n", System.currentTimeMillis(), sb.toString(), apdu.getSW());
+        }
+    }
+}
